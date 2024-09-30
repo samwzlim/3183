ae9738b9932fa6dc164ad1879b8069ea594690aa
Samuel Wei Ze Lim <slim0130@student.monash.edu>
2024-09-30 15:31:47 +0800

    update

--- core/solver.py
+++ core/solver.py
@@ -1,130 +1,292 @@
 """
-StarGAN v2 Solver with Perceptual Loss Integration
+StarGAN v2
+Copyright (c) 2020-present NAVER Corp.
+
+This work is licensed under the Creative Commons Attribution-NonCommercial
+4.0 International License. To view a copy of this license, visit
+http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 """
 
-import copy
-import math
+import os
+from os.path import join as ospj
+import time
+import datetime
+from munch import Munch
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
-from munch import Munch
-from torchvision import models
 
-from core.wing import FAN
+from core.model import build_model
+from core.checkpoint import CheckpointIO
+from core.data_loader import InputFetcher
+import core.utils as utils
+from metrics.eval import calculate_metrics
 
 
-class Solver:
+class Solver(nn.Module):
     def __init__(self, args):
+        super().__init__()
         self.args = args
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.build_model()
-        self.optimizer_G = torch.optim.Adam(self.nets.generator.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
-        self.optimizer_D = torch.optim.Adam(self.nets.discriminator.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
 
-        # Load pre-trained VGG19 for perceptual loss
-        self.vgg = models.vgg19(pretrained=True).features[:16].eval().to(self.device)
-        for param in self.vgg.parameters():
-            param.requires_grad = False  # Freeze VGG weights
+        self.nets, self.nets_ema = build_model(args)
+        # below setattrs are to make networks be children of Solver, e.g., for self.to(self.device)
+        for name, module in self.nets.items():
+            utils.print_network(module, name)
+            setattr(self, name, module)
+        for name, module in self.nets_ema.items():
+            setattr(self, name + '_ema', module)
 
-    def build_model(self):
-        """Build Generator, Discriminator, and other components"""
-        from core.model import Generator, MappingNetwork, StyleEncoder, Discriminator, FAN
-        self.nets = Munch(generator=Generator(args.img_size, args.style_dim, args.w_hpf),
-                          mapping_network=MappingNetwork(args.latent_dim, args.style_dim, args.num_domains),
-                          style_encoder=StyleEncoder(args.img_size, args.style_dim, args.num_domains),
-                          discriminator=Discriminator(args.img_size, args.num_domains))
-        self.nets.generator.to(self.device)
-        self.nets.mapping_network.to(self.device)
-        self.nets.style_encoder.to(self.device)
-        self.nets.discriminator.to(self.device)
+        if args.mode == 'train':
+            self.optims = Munch()
+            for net in self.nets.keys():
+                if net == 'fan':
+                    continue
+                self.optims[net] = torch.optim.Adam(
+                    params=self.nets[net].parameters(),
+                    lr=args.f_lr if net == 'mapping_network' else args.lr,
+                    betas=[args.beta1, args.beta2],
+                    weight_decay=args.weight_decay)
 
-        if args.w_hpf > 0:
-            self.fan = FAN(fname_pretrained=args.wing_path).eval().to(self.device)
-            self.nets.fan = self.fan
+            self.ckptios = [
+                CheckpointIO(ospj(args.checkpoint_dir, '{:06d}_nets.ckpt'), data_parallel=True, **self.nets),
+                CheckpointIO(ospj(args.checkpoint_dir, '{:06d}_nets_ema.ckpt'), data_parallel=True, **self.nets_ema),
+                CheckpointIO(ospj(args.checkpoint_dir, '{:06d}_optims.ckpt'), **self.optims)]
+        else:
+            self.ckptios = [CheckpointIO(ospj(args.checkpoint_dir, '{:06d}_nets_ema.ckpt'), data_parallel=True, **self.nets_ema)]
 
-    def perceptual_loss(self, x, y):
-        """
-        Compute perceptual loss between x and y using VGG19.
-        Args:
-            x: Generated image
-            y: Real image
-        Returns:
-            Perceptual loss (L1 loss between feature maps)
-        """
-        x_features = self.vgg(x)
-        y_features = self.vgg(y)
-        return F.l1_loss(x_features, y_features)
+        self.to(self.device)
+        for name, network in self.named_children():
+            # Do not initialize the FAN parameters
+            if ('ema' not in name) and ('fan' not in name):
+                print('Initializing %s...' % name)
+                network.apply(utils.he_init)
+
+    def _save_checkpoint(self, step):
+        for ckptio in self.ckptios:
+            ckptio.save(step)
+
+    def _load_checkpoint(self, step):
+        for ckptio in self.ckptios:
+            ckptio.load(step)
+
+    def _reset_grad(self):
+        for optim in self.optims.values():
+            optim.zero_grad()
 
     def train(self, loaders):
-        """Training loop."""
-        self.train_iter = iter(loaders.src)
-        self.val_iter = iter(loaders.val)
+        args = self.args
+        nets = self.nets
+        nets_ema = self.nets_ema
+        optims = self.optims
 
-        for i in range(self.args.total_iters):
-            # Fetch training batch
-            try:
-                x_real, y_org = next(self.train_iter)
-            except StopIteration:
-                self.train_iter = iter(loaders.src)
-                x_real, y_org = next(self.train_iter)
+        # fetch random validation images for debugging
+        fetcher = InputFetcher(loaders.src, loaders.ref, args.latent_dim, 'train')
+        fetcher_val = InputFetcher(loaders.val, None, args.latent_dim, 'val')
+        inputs_val = next(fetcher_val)
 
-            x_real = x_real.to(self.device)
-            y_org = y_org.to(self.device)
+        # resume training if necessary
+        if args.resume_iter > 0:
+            self._load_checkpoint(args.resume_iter)
 
-            # Train Discriminator
-            d_loss = self.compute_d_loss(x_real, y_org)
-            self.optimizer_D.zero_grad()
+        # remember the initial value of ds weight
+        initial_lambda_ds = args.lambda_ds
+
+        print('Start training...')
+        start_time = time.time()
+        for i in range(args.resume_iter, args.total_iters):
+            # fetch images and labels
+            inputs = next(fetcher)
+            x_real, y_org = inputs.x_src, inputs.y_src
+            x_ref, x_ref2, y_trg = inputs.x_ref, inputs.x_ref2, inputs.y_ref
+            z_trg, z_trg2 = inputs.z_trg, inputs.z_trg2
+
+            masks = nets.fan.get_heatmap(x_real) if args.w_hpf > 0 else None
+
+            # train the discriminator
+            d_loss, d_losses_latent = compute_d_loss(
+                nets, args, x_real, y_org, y_trg, z_trg=z_trg, masks=masks)
+            self._reset_grad()
             d_loss.backward()
-            self.optimizer_D.step()
+            optims.discriminator.step()
 
-            # Train Generator
-            g_loss = self.compute_g_loss(x_real, y_org)
-            self.optimizer_G.zero_grad()
+            d_loss, d_losses_ref = compute_d_loss(
+                nets, args, x_real, y_org, y_trg, x_ref=x_ref, masks=masks)
+            self._reset_grad()
+            d_loss.backward()
+            optims.discriminator.step()
+
+            # train the generator
+            g_loss, g_losses_latent = compute_g_loss(
+                nets, args, x_real, y_org, y_trg, z_trgs=[z_trg, z_trg2], masks=masks)
+            self._reset_grad()
             g_loss.backward()
-            self.optimizer_G.step()
+            optims.generator.step()
+            optims.mapping_network.step()
+            optims.style_encoder.step()
 
-            # Logging
-            if (i + 1) % self.args.print_every == 0:
-                print(f"Iter [{i+1}/{self.args.total_iters}] | d_loss: {d_loss.item():.4f} | g_loss: {g_loss.item():.4f}")
+            g_loss, g_losses_ref = compute_g_loss(
+                nets, args, x_real, y_org, y_trg, x_refs=[x_ref, x_ref2], masks=masks)
+            self._reset_grad()
+            g_loss.backward()
+            optims.generator.step()
 
-            # Save the model checkpoints
-            if (i + 1) % self.args.save_every == 0:
-                torch.save(self.nets.generator.state_dict(), f'expr/checkpoints/generator_{i+1}.pth')
-                torch.save(self.nets.discriminator.state_dict(), f'expr/checkpoints/discriminator_{i+1}.pth')
+            # compute moving average of network parameters
+            moving_average(nets.generator, nets_ema.generator, beta=0.999)
+            moving_average(nets.mapping_network, nets_ema.mapping_network, beta=0.999)
+            moving_average(nets.style_encoder, nets_ema.style_encoder, beta=0.999)
 
-    def compute_d_loss(self, x_real, y_org):
-        """Compute loss for the discriminator."""
-        x_fake = self.nets.generator(x_real, y_org)
-        out_real = self.nets.discriminator(x_real, y_org)
-        out_fake = self.nets.discriminator(x_fake.detach(), y_org)
+            # decay weight for diversity sensitive loss
+            if args.lambda_ds > 0:
+                args.lambda_ds -= (initial_lambda_ds / args.ds_iter)
 
-        # Adversarial loss
-        d_loss_real = torch.mean(F.relu(1.0 - out_real))
-        d_loss_fake = torch.mean(F.relu(1.0 + out_fake))
+            # print out log info
+            if (i+1) % args.print_every == 0:
+                elapsed = time.time() - start_time
+                elapsed = str(datetime.timedelta(seconds=elapsed))[:-7]
+                log = "Elapsed time [%s], Iteration [%i/%i], " % (elapsed, i+1, args.total_iters)
+                all_losses = dict()
+                for loss, prefix in zip([d_losses_latent, d_losses_ref, g_losses_latent, g_losses_ref],
+                                        ['D/latent_', 'D/ref_', 'G/latent_', 'G/ref_']):
+                    for key, value in loss.items():
+                        all_losses[prefix + key] = value
+                all_losses['G/lambda_ds'] = args.lambda_ds
+                log += ' '.join(['%s: [%.4f]' % (key, value) for key, value in all_losses.items()])
+                print(log)
 
-        return d_loss_real + d_loss_fake
+            # generate images for debugging
+            if (i+1) % args.sample_every == 0:
+                os.makedirs(args.sample_dir, exist_ok=True)
+                utils.debug_image(nets_ema, args, inputs=inputs_val, step=i+1)
 
-    def compute_g_loss(self, x_real, y_org):
-        """Compute loss for the generator."""
-        # Adversarial loss
-        x_fake = self.nets.generator(x_real, y_org)
-        out_fake = self.nets.discriminator(x_fake, y_org)
-        g_loss_adv = -torch.mean(out_fake)
+            # save model checkpoints
+            if (i+1) % args.save_every == 0:
+                self._save_checkpoint(step=i+1)
 
-        # Perceptual Loss
-        g_loss_perc = self.perceptual_loss(x_fake, x_real) * self.args.lambda_perc
-
-        return g_loss_adv + g_loss_perc
+            # compute FID and LPIPS if necessary
+            if (i+1) % args.eval_every == 0:
+                calculate_metrics(nets_ema, args, i+1, mode='latent')
+                calculate_metrics(nets_ema, args, i+1, mode='reference')
 
     @torch.no_grad()
     def sample(self, loaders):
-        """Sample images for testing."""
-        self.nets.generator.eval()
-        for x_src, y_src in loaders.src:
-            x_src = x_src.to(self.device)
-            y_src = y_src.to(self.device)
+        args = self.args
+        nets_ema = self.nets_ema
+        os.makedirs(args.result_dir, exist_ok=True)
+        self._load_checkpoint(args.resume_iter)
 
-            x_fake = self.nets.generator(x_src, y_src)
-            x_fake = (x_fake + 1) / 2  # Convert back to [0, 1]
-            torchvision.utils.save_image(x_fake, f'expr/samples/{i+1}_fake.png')
+        src = next(InputFetcher(loaders.src, None, args.latent_dim, 'test'))
+        ref = next(InputFetcher(loaders.ref, None, args.latent_dim, 'test'))
+
+        fname = ospj(args.result_dir, 'reference.jpg')
+        print('Working on {}...'.format(fname))
+        utils.translate_using_reference(nets_ema, args, src.x, ref.x, ref.y, fname)
+
+        fname = ospj(args.result_dir, 'video_ref.mp4')
+        print('Working on {}...'.format(fname))
+        utils.video_ref(nets_ema, args, src.x, ref.x, ref.y, fname)
+
+    @torch.no_grad()
+    def evaluate(self):
+        args = self.args
+        nets_ema = self.nets_ema
+        resume_iter = args.resume_iter
+        self._load_checkpoint(args.resume_iter)
+        calculate_metrics(nets_ema, args, step=resume_iter, mode='latent')
+        calculate_metrics(nets_ema, args, step=resume_iter, mode='reference')
+
+
+def compute_d_loss(nets, args, x_real, y_org, y_trg, z_trg=None, x_ref=None, masks=None):
+    assert (z_trg is None) != (x_ref is None)
+    # with real images
+    x_real.requires_grad_()
+    out = nets.discriminator(x_real, y_org)
+    loss_real = adv_loss(out, 1)
+    loss_reg = r1_reg(out, x_real)
+
+    # with fake images
+    with torch.no_grad():
+        if z_trg is not None:
+            s_trg = nets.mapping_network(z_trg, y_trg)
+        else:  # x_ref is not None
+            s_trg = nets.style_encoder(x_ref, y_trg)
+
+        x_fake = nets.generator(x_real, s_trg, masks=masks)
+    out = nets.discriminator(x_fake, y_trg)
+    loss_fake = adv_loss(out, 0)
+
+    loss = loss_real + loss_fake + args.lambda_reg * loss_reg
+    return loss, Munch(real=loss_real.item(),
+                       fake=loss_fake.item(),
+                       reg=loss_reg.item())
+
+
+def compute_g_loss(nets, args, x_real, y_org, y_trg, z_trgs=None, x_refs=None, masks=None):
+    assert (z_trgs is None) != (x_refs is None)
+    if z_trgs is not None:
+        z_trg, z_trg2 = z_trgs
+    if x_refs is not None:
+        x_ref, x_ref2 = x_refs
+
+    # adversarial loss
+    if z_trgs is not None:
+        s_trg = nets.mapping_network(z_trg, y_trg)
+    else:
+        s_trg = nets.style_encoder(x_ref, y_trg)
+
+    x_fake = nets.generator(x_real, s_trg, masks=masks)
+    out = nets.discriminator(x_fake, y_trg)
+    loss_adv = adv_loss(out, 1)
+
+    # style reconstruction loss
+    s_pred = nets.style_encoder(x_fake, y_trg)
+    loss_sty = torch.mean(torch.abs(s_pred - s_trg))
+
+    # diversity sensitive loss
+    if z_trgs is not None:
+        s_trg2 = nets.mapping_network(z_trg2, y_trg)
+    else:
+        s_trg2 = nets.style_encoder(x_ref2, y_trg)
+    x_fake2 = nets.generator(x_real, s_trg2, masks=masks)
+    x_fake2 = x_fake2.detach()
+    loss_ds = torch.mean(torch.abs(x_fake - x_fake2))
+
+    # cycle-consistency loss
+    masks = nets.fan.get_heatmap(x_fake) if args.w_hpf > 0 else None
+    s_org = nets.style_encoder(x_real, y_org)
+    x_rec = nets.generator(x_fake, s_org, masks=masks)
+    loss_cyc = torch.mean(torch.abs(x_rec - x_real))
+
+    loss = loss_adv + args.lambda_sty * loss_sty \
+        - args.lambda_ds * loss_ds + args.lambda_cyc * loss_cyc
+    return loss, Munch(adv=loss_adv.item(),
+                       sty=loss_sty.item(),
+                       ds=loss_ds.item(),
+                       cyc=loss_cyc.item())
+
+
+def moving_average(model, model_test, beta=0.999):
+    for param, param_test in zip(model.parameters(), model_test.parameters()):
+        param_test.data = torch.lerp(param.data, param_test.data, beta)
+
+
+def adv_loss(logits, target):
+    assert target in [1, 0]
+    targets = torch.full_like(logits, fill_value=target)
+    loss = F.binary_cross_entropy_with_logits(logits, targets)
+    return loss
+
+
+def r1_reg(d_out, x_in):
+    # zero-centered gradient penalty for real images
+    batch_size = x_in.size(0)
+    grad_dout = torch.autograd.grad(
+        outputs=d_out.sum(), inputs=x_in,
+        create_graph=True, retain_graph=True, only_inputs=True
+    )[0]
+    grad_dout2 = grad_dout.pow(2)
+    assert(grad_dout2.size() == x_in.size())
+    reg = 0.5 * grad_dout2.view(batch_size, -1).sum(1).mean(0)
+    return reg
